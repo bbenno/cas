cc2ca8730d18a03e63cbdec756091a2bb7985921
Benno Bielmeier <benno.bielmeier@st.oth-regensburg.de>
2020-10-02 23:16:03 +0200

    add databasecleaner gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -47,6 +47,19 @@ RSpec.configure do |config|
   # add `FactoryBot` methods
   config.include FactoryBot::Syntax::Methods
 
+  # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  # start the transaction strategy as examples are run
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
