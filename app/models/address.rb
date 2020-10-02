fdf6cfda5082bf64fc25f0926ca8ea664c519b57
Benno Bielmeier <benno.bielmeier@st.oth-regensburg.de>
2020-10-03 00:13:42 +0200

    furhter presence validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,6 @@
 class Address < ApplicationRecord
   validates :street_address, presence: true, format: { with: /\d+/, message: 'missing house number' }
+  validates :city, presence: true
+  validates :zip_code, presence: true
+  validates :country_code, presence: true, length: { is: 2 }
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -10,5 +10,21 @@ RSpec.describe Address, type: :model do
       address = Address.new(street_address: 'Long Hill Street').save
       expect(address).to eq(false)
     end
+
+    it 'ensures the presence of city' do
+      should validate_presence_of(:city)
+    end
+
+    it 'ensures the presence of zip_code' do
+      should validate_presence_of(:zip_code)
+    end
+
+    it 'ensures the presence of country_code' do
+      should validate_presence_of(:country_code)
+    end
+
+    it 'ensures that country_code has length of two' do
+      should validate_length_of(:country_code).is_equal_to(2)
+    end
   end
 end
