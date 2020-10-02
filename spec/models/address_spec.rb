2cf995e2643b0c6fc14cab977934e9ded0fc42af
Benno Bielmeier <benno.bielmeier@st.oth-regensburg.de>
2020-10-02 22:49:53 +0200

    simplify validation with shoulda-matchers

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -2,9 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Address, type: :model do
   context 'validations' do
-    it 'ensures the presence of street' do
-      address = Address.new(street: '').save
-      expect(address).to eq(false)
-    end
+    it { should validate_presence_of(:street) }
   end
 end
