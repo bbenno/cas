b83fab151649f78fb5a2c5ea74f0d21910851913
Benno Bielmeier <benno.bielmeier@st.oth-regensburg.de>
2020-10-03 14:53:01 +0200

    letterable accepts nested attributes

--- app/models/concerns/letterable.rb
+++ app/models/concerns/letterable.rb
@@ -3,5 +3,6 @@ module Letterable
 
   included do
     has_one :address, as: :addressable
+    accepts_nested_attributes_for :address, allow_destroy: true
   end
 end
--- spec/support/concerns/letterable.rb
+++ spec/support/concerns/letterable.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 shared_examples_for :letterable do
   let(:model) { create(described_class.to_s.underscore) }
 
-  it 'has a address' do
-    expect { model.address }.to_not raise_error
-  end
+  it { should have_one(:address) }
+  it { should accept_nested_attributes_for(:address).allow_destroy(true) }
 end
