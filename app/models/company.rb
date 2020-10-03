a4c8d2d0a9e8c0e9a8406f4bb50f73297dbe696f
Benno Bielmeier <benno.bielmeier@st.oth-regensburg.de>
2020-10-03 15:49:51 +0200

    fix case insensitive uniqueness of company name

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,6 @@
 class Company < ApplicationRecord
   include Letterable
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
   enum status: %i[active inactive archived]
 end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Company, type: :model do
 
   context 'validations' do
     it { should validate_presence_of(:name) }
-    it { should validate_uniqueness_of(:name) }
+    it { should validate_uniqueness_of(:name).case_insensitive  }
   end
 
   it { should define_enum_for(:status).with_values(%i[active inactive archived]) }
