2ca7b51d4107b66a9fc10dc7368c0367e3db0274
Benno Bielmeier <benno.bielmeier@st.oth-regensburg.de>
2020-10-03 10:47:00 +0200

    shorten rspec validations

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -2,36 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Address, type: :model do
   context 'validations' do
-    it 'ensures the presence of street' do
-      should validate_presence_of(:street_address)
-    end
+    it { should validate_presence_of(:street_address) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:zip_code) }
+    it { should validate_presence_of(:country_code) }
+    it { should validate_length_of(:country_code).is_equal_to(2) }
+    it { should belong_to(:addressable) }
 
     it 'ensures that street contains at least one number' do
       address = Address.new(street_address: Faker::Address.street_name).save
       expect(address).to eq(false)
     end
 
-    it 'ensures the presence of city' do
-      should validate_presence_of(:city)
-    end
-
-    it 'ensures the presence of zip_code' do
-      should validate_presence_of(:zip_code)
-    end
-
-    it 'ensures the presence of country_code' do
-      should validate_presence_of(:country_code)
-    end
-
-    it 'ensures that country_code has length of two' do
-      should validate_length_of(:country_code).is_equal_to(2)
-    end
-
     it 'ensures that country_code is valid ISO3166 code' do
       address = build(:address, country_code: 'ZZ')
       expect(address.valid?).to eq(false)
     end
-
-    it { should belong_to(:addressable) }
   end
 end
